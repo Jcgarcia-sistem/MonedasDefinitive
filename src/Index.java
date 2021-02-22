13169984fa3604091609bb552a12e10a3dd227a9
jonny <carlos_garcia_sistemas@outlook.com>
2021-02-22 00:53:41 -0600

    Se crea clase de ingreso de datos

--- src/Index.java
+++ src/Index.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Index {
     public static void main(String[] args) {
+
+
+    IntoDates dates = new IntoDates();
+
+/**
+ * Ingreso de datos
+ * @param mount es  la cantidad a convertir
+ * @param type es el tipo de moneda a convertir
+ * */
+
+        Scanner cant = new Scanner(System.in);
+       dates.mount();
+       double mount = cant.nextDouble();
+        System.out.println(mount);
 
     }
 }
---
+++ src/IntoDates.java
@@ -0,0 +1,21 @@
+public class IntoDates {
+    private int election;
+    /**
+     * Este modulo controla el menu que ve el usuario
+     *
+     * */
+    IntoDates(){}
+
+    public void mount(){
+        System.out.println("Ingresa el monto a convertir ");
+    }
+    public void type(){
+        System.out.println("Ingresa a que moneda deseas convertir  de MNX ");
+        System.out.println("puedes seleccionar una de las siguientes opciones: ");
+
+
+
+
+
+    }
+}
