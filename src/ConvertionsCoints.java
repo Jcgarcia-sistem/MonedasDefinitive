6d59be27fd7e849ccaad5fe5dd6677a0ea879aae
jonny <carlos_garcia_sistemas@outlook.com>
2021-02-21 20:17:31 -0600

    Se agregan atributos a super clase asi como metodos de conversion de peso a moneda y viserversa

--- src/ConvertionsCoints.java
+++ src/ConvertionsCoints.java
@@ -1,2 +1,21 @@
 public class ConvertionsCoints {
+
+    String type;
+    double valueCoin;
+
+
+    ConvertionsCoints(String type, double valueCoin){
+        this.type = type;
+        this.valueCoin = valueCoin;
+    }
+
+    public double conveDePesoa(double userCoin){
+        valueCoin = userCoin / valueCoin;
+        return valueCoin;
+    }
+
+    public double converDeCoina(double userCoin){
+        valueCoin = userCoin * valueCoin;
+        return  valueCoin;
+    }
 }
